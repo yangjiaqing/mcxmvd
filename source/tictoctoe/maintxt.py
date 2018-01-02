f809e2dd6b19cbc4046b46c6dc58ae15645a29fb
xknxknqq <1553730177@qq.com>
2018-01-02 10:56:48 +0800

    first opencv tictactoe version

---
+++ source/tictoctoe/maintxt.py
@@ -0,0 +1,21 @@
+import tictactoe as tic
+
+win = False
+while win is False:
+    #acertar verificacao, sair do loop imediatamente se houve ganhador
+    tic.getPlayerMark(1)
+    tic.displayboard()
+    win = tic.verifyWinner(1)
+    if win=='EMPATE':
+        print('no body win')
+        break
+    if win:
+        print('You win !')
+        break
+    tic.inteligence(2)
+    win = tic.verifyWinner(2)
+    print("computer played")
+    tic.displayboard()
+    if win:
+        print('computer win')
+        break
